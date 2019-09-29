dc50b462122761874c7ff155bcc29b0942206b23
Miguel Clark <miguelclark@hotmail.com>
2019-09-28 23:38:02 -0700

    Second Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyC_C7sumyNzZacd5TDwnEfPjup_JQMI8ig",
+    authDomain: "todo-app-cb87a.firebaseapp.com",
+    databaseURL: "https://todo-app-cb87a.firebaseio.com",
+    projectId: "todo-app-cb87a",
+    storageBucket: "todo-app-cb87a.appspot.com",
+    messagingSenderId: "966560164024",
+    appId: "1:966560164024:web:cc3cd8d2a2219996a82c4d"
+  }
 };
 
 /*
